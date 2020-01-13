0e42f0082cc389a07a3d298ee731b4849301b365
Joonas Meriläinen <merilainenjoonas@gmail.com>
2020-01-13 16:45:57 +0200

    Update path

--- src/Console.php
+++ src/Console.php
@@ -9,7 +9,7 @@ class Console
         (new JsonBuilder(realpath(__DIR__ . '/..')))
             ->from('packages')
             ->name('jmerilainen/packagist')
-            ->homepage('https://packagist.jmerilainen.fi/')
+            ->homepage('https://jmerilainen-packagist-storage.s3.eu-north-1.amazonaws.com/')
             ->save('.satis.json');
     }
 }
