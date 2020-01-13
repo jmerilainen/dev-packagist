cb2c5d67e5021638508214a70437dbb32b94e14e
Joonas Meriläinen <merilainenjoonas@gmail.com>
2020-01-13 17:01:00 +0200

    Test

--- src/Console.php
+++ src/Console.php
@@ -9,7 +9,7 @@ class Console
         (new JsonBuilder(realpath(__DIR__ . '/..')))
             ->from('packages')
             ->name('jmerilainen/packagist')
-            ->homepage('https://jmerilainen-packagist-storage.s3.eu-north-1.amazonaws.com/')
+            ->homepage('https://jmerilainen-packagist-storage.s3.eu-north-1.amazonaws.com')
             ->save('.satis.json');
     }
 }
